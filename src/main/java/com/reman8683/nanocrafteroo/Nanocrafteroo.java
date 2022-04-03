6a9c6dcec362716050c2f40362f80d6b39132552
REMAN <reman8683@gmail.com>
2022-04-04 04:28:47 +0900

    Change Somethings

--- src/main/java/com/reman8683/nanocrafteroo/Nanocrafteroo.java
+++ src/main/java/com/reman8683/nanocrafteroo/Nanocrafteroo.java
@@ -2,7 +2,10 @@ package com.reman8683.nanocrafteroo;
 
 import fi.dy.masa.malilib.event.InitializationHandler;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
+@Environment(EnvType.CLIENT)
 public class Nanocrafteroo implements ClientModInitializer {
 
 	@Override
